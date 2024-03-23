6aae0fd556f6000935fb915fa09925c121d9f9b6
Rishav De <rishavde@scc2.bu.edu>
2024-03-23 18:24:36 +0000

    added extra functionalities

--- src/pke/examples/simple-real-numbers-ICSG-test.cpp
+++ src/pke/examples/simple-real-numbers-ICSG-test.cpp
@@ -125,7 +125,7 @@ int main() {
     cc->Enable(PKE);
     cc->Enable(KEYSWITCH);
     cc->Enable(LEVELEDSHE);
-    std::cout << "CKKS scheme is using ring dimension " << cc->GetRingDimension() << std::endl << std::endl;
+    //std::cout << "CKKS scheme is using ring dimension " << cc->GetRingDimension() << std::endl << std::endl;
 
     // B. Step 2: Key Generation
     /* B1) Generate encryption keys.
@@ -176,8 +176,8 @@ int main() {
     Plaintext ptxt1 = cc->MakeCKKSPackedPlaintext(x1);
     Plaintext ptxt2 = cc->MakeCKKSPackedPlaintext(x2);
 
-    std::cout << "Input x1: " << ptxt1 << std::endl;
-    std::cout << "Input x2: " << ptxt2 << std::endl;
+    //std::cout << "Input x1: " << ptxt1 << std::endl;
+    //std::cout << "Input x2: " << ptxt2 << std::endl;
 
     // Encrypt the encoded vectors
     auto c1 = cc->Encrypt(keys.publicKey, ptxt1);
@@ -186,67 +186,70 @@ int main() {
     // Step 4: Evaluation
 
     // Homomorphic addition
-    std::cout << "Before EvalAdd in " << __FILE__ << std::endl;
+    //std::cout << "Before EvalAdd in " << __FILE__ << std::endl;
     auto cAdd = cc->EvalAdd(c1, c2);
-    std::cout << "After EvalAdd in " << __FILE__ << std::endl;
+    //std::cout << "After EvalAdd in " << __FILE__ << std::endl;
     // Homomorphic subtraction
-    auto cSub = cc->EvalSub(c1, c2);
+    //auto cSub = cc->EvalSub(c1, c2);
 
     // Homomorphic scalar multiplication
-    auto cScalar = cc->EvalMult(c1, 4.0);
+    //auto cScalar = cc->EvalMult(c1, 4.0);
 
     // Homomorphic multiplication
-    auto cMul = cc->EvalMult(c1, c2);
+    //auto cMul = cc->EvalMult(c1, c2);
 
     // Homomorphic rotations
-    auto cRot1 = cc->EvalRotate(c1, 1);
-    auto cRot2 = cc->EvalRotate(c1, -2);
+    //auto cRot1 = cc->EvalRotate(c1, 1);
+    //auto cRot2 = cc->EvalRotate(c1, -2);
 
     // Step 5: Decryption and output
-    Plaintext result;
+    //Plaintext result;
     // We set the cout precision to 8 decimal digits for a nicer output.
     // If you want to see the error/noise introduced by CKKS, bump it up
     // to 15 and it should become visible.
-    std::cout.precision(8);
+    //std::cout.precision(8);
 
-    std::cout << std::endl << "Results of homomorphic computations: " << std::endl;
+    //std::cout << std::endl << "Results of homomorphic computations: " << std::endl;
 
-    cc->Decrypt(keys.secretKey, c1, &result);
-    result->SetLength(batchSize);
-    std::cout << "x1 = " << result;
-    std::cout << "Estimated precision in bits: " << result->GetLogPrecision() << std::endl;
+    //cc->Decrypt(keys.secretKey, c1, &result);
+    //result->SetLength(batchSize);
+    //std::cout << "x1 = " << result;
+    //std::cout << "Estimated precision in bits: " << result->GetLogPrecision() << std::endl;
 
     // Decrypt the result of addition
-    cc->Decrypt(keys.secretKey, cAdd, &result);
-    result->SetLength(batchSize);
-    std::cout << "x1 + x2 = " << result;
-    std::cout << "Estimated precision in bits: " << result->GetLogPrecision() << std::endl;
+    //cc->Decrypt(keys.secretKey, cAdd, &result);
+    //result->SetLength(batchSize);
+    //std::cout << "x1 + x2 = " << result;
+    //std::cout << "Estimated precision in bits: " << result->GetLogPrecision() << std::endl;
 
     // Decrypt the result of subtraction
-    cc->Decrypt(keys.secretKey, cSub, &result);
-    result->SetLength(batchSize);
-    std::cout << "x1 - x2 = " << result << std::endl;
+    //cc->Decrypt(keys.secretKey, cSub, &result);
+    //result->SetLength(batchSize);
+    //std::cout << "x1 - x2 = " << result << std::endl;
 
     // Decrypt the result of scalar multiplication
-    cc->Decrypt(keys.secretKey, cScalar, &result);
-    result->SetLength(batchSize);
-    std::cout << "4 * x1 = " << result << std::endl;
+    //cc->Decrypt(keys.secretKey, cScalar, &result);
+    //result->SetLength(batchSize);
+    //std::cout << "4 * x1 = " << result << std::endl;
 
     // Decrypt the result of multiplication
-    cc->Decrypt(keys.secretKey, cMul, &result);
-    result->SetLength(batchSize);
-    std::cout << "x1 * x2 = " << result << std::endl;
+    //cc->Decrypt(keys.secretKey, cMul, &result);
+    //result->SetLength(batchSize);
+    //std::cout << "x1 * x2 = " << result << std::endl;
 
     // Decrypt the result of rotations
 
-    cc->Decrypt(keys.secretKey, cRot1, &result);
-    result->SetLength(batchSize);
-    std::cout << std::endl << "In rotations, very small outputs (~10^-10 here) correspond to 0's:" << std::endl;
-    std::cout << "x1 rotate by 1 = " << result << std::endl;
+    //cc->Decrypt(keys.secretKey, cRot1, &result);
+    //result->SetLength(batchSize);
+    //std::cout << std::endl << "In rotations, very small outputs (~10^-10 here) correspond to 0's:" << std::endl;
+    //std::cout << "x1 rotate by 1 = " << result << std::endl;
 
-    cc->Decrypt(keys.secretKey, cRot2, &result);
-    result->SetLength(batchSize);
-    std::cout << "x1 rotate by -2 = " << result << std::endl;
-
+    //cc->Decrypt(keys.secretKey, cRot2, &result);
+    //result->SetLength(batchSize);
+    //std::cout << "x1 rotate by -2 = " << result << std::endl;
+    for (const auto& pair : ChineseRemainderTransformFTT<NativeVector>::m_rootOfUnityReverseTableByModulus){
+        std::cout << "Key: " <<  pair.first << "\nValue: " << pair.second << std::endl;
+	std::cout << "-----------------------------------------------------------------------" << std::endl;
+    }
     return 0;
 }
